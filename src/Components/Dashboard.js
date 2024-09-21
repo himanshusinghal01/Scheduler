714ed1e5558b6cc79073fc068baa234d35ed4eba
Himanshu Singhal <himanshusinghalhs0111@gmail.com>
2024-09-21 18:19:16 +0530

    Add files via upload

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+    <div className='flex justify-center items-center text-white text-3xl h-full'>Dashboard</div>
+        Welcome to StudyNotion!
+    </div>
+  )
+}
+
+export default Dashboard
